796a9d598d8ee980cbb496b498853463b9197f10
Zeki Özen <1252016+zeki-ozen@users.noreply.github.com>
2025-06-13 23:04:10 +0300

    Add files via upload

---
+++ W7/Method_Hiding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W7_OOP
+{
+
+
+    class Base
+    {
+        public void A() { Console.WriteLine("Base:A"); }
+    }
+    class Derived : Base
+    {
+        // method hiding using with new keyword
+        public new void A()
+        {
+            Console.WriteLine("Derived:A");
+        }
+    }
+}
+
+
+
+
---
+++ W7/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace W7_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Student student = new Student();
+            student.studentID = 1;
+            student.studentName = "Fatih";
+            student.studentSurname = "Sungur";
+            */
+
+            /*
+            Student student = new Student(1, "Fatih", "Sungur");
+            student.TakeExam();
+
+
+
+            student = null; // Referansı bırak
+            GC.Collect();   // Zorla GC'yi çağır
+            GC.WaitForPendingFinalizers(); // Yıkıcıların çalışmasını bekle
+
+            Console.WriteLine("Program bitti.");
+            */
+
+            // We can not create an instance of interface and abstract.
+            // Person prsn = new Person();
+
+
+            Worker isci = new Worker();
+            isci.Work();
+            isci.GetSalary();
+            isci.LeaveVacation();
+
+
+            /**** Method Hiding Example ****/
+            // inheritance of Base class
+            Base b = new Base();
+            b.A(); // Output: Base:A
+
+            // inheritance of Derived class
+            Derived d = new Derived();
+            d.A(); // Output: Derived:A
+
+            // Method hiding
+            // notice that reference type is Base, but object type is Derived
+            Base bd = new Derived();
+            bd.A(); // Output: Base:A (method hiding, not overriding)
+            Console.ReadLine();
+
+        }
+    }
+}
