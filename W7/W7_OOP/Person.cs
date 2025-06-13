3cf537fafbc00fd4afc35c26e476cd99413779f1
Zeki Özen <1252016+zeki-ozen@users.noreply.github.com>
2025-06-13 21:40:56 +0300

    Add files via upload

---
+++ W7/W7_OOP/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W7_OOP
+{
+    internal interface Person
+    {
+        void Work();
+        void GetSalary();
+        void LeaveVacation();
+  
+    }
+
+    internal class Worker : Person
+    {
+        public void Work()
+        {
+            Console.WriteLine("Worker is working");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Worker is getting his/her salary.");
+        }
+
+        public void LeaveVacation()
+        {
+            Console.WriteLine("Worker is now at the vacation.");
+        }
+
+    }
+
+}
---
+++ W7/W7_OOP/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace W7_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Student student = new Student();
+            student.studentID = 1;
+            student.studentName = "Fatih";
+            student.studentSurname = "Sungur";
+            */
+
+            /*
+            Student student = new Student(1, "Fatih", "Sungur");
+            student.TakeExam();
+
+
+
+            student = null; // Referansı bırak
+            GC.Collect();   // Zorla GC'yi çağır
+            GC.WaitForPendingFinalizers(); // Yıkıcıların çalışmasını bekle
+
+            Console.WriteLine("Program bitti.");
+            */
+
+            // We can not create an instance of interface and abstract.
+            // Person prsn = new Person();
+
+
+            Worker isci = new Worker();
+            isci.Work();
+            isci.GetSalary();
+            isci.LeaveVacation();
+
+
+        }
+    }
+}
